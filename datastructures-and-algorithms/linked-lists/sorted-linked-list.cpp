b7a2553c9ec3907660e765587eb86044bd120345
eclesio <eclesiomelo.1@gmail.com>
2020-09-01 10:20:36 -0400

    chore: updating linked lists

--- datastructures-and-algorithms/linked-lists/sorted-linked-list.cpp
+++ datastructures-and-algorithms/linked-lists/sorted-linked-list.cpp
@@ -6,12 +6,14 @@ class Node
 {
 public:
   int value;
+  Node *prev;
   Node *next;
 
   Node(int v)
   {
     value = v;
     next = NULL;
+    prev = NULL;
   }
 };
 
@@ -27,14 +29,16 @@ public:
   void add(int value)
   {
     Node *tmp = new Node(value);
+
     Node *n = head;
 
     if (n == NULL)
-      head = new Node(value);
+      head = end = new Node(value);
     else
     {
       if (n->value > value)
       {
+        n->prev = tmp;
         tmp->next = n;
         head = tmp;
       }
@@ -52,7 +56,13 @@ public:
         }
 
         tmp->next = n->next;
+        tmp->prev = n;
         n->next = tmp;
+
+        if (tmp->next == NULL)
+          end = tmp;
+        else
+          tmp->next->prev = tmp;
       }
     }
   }
@@ -70,8 +80,22 @@ public:
     cout << endl;
   }
 
+  void printBack()
+  {
+    Node *n = end;
+
+    while (n != NULL)
+    {
+      cout << n->value << " -> ";
+      n = n->prev;
+    }
+
+    cout << endl;
+  }
+
 private:
   Node *head;
+  Node *end;
 };
 
 int main()
@@ -82,7 +106,11 @@ int main()
   for (int i = 0; i < 10; i++)
     sl.add(rand() % 100 + 1);
 
+  cout << "Ascending order" << endl;
   sl.print();
+
+  cout << "Descending order" << endl;
+  sl.printBack();
 
   return 0;
 }
